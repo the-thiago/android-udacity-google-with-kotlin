40b5ec84dd8e21077428258f62a1a67ce3b3071b
Thiago Moreira de Souza <thiago.moreira.souza@gmail.com>
2021-02-10 18:01:37 -0300

    Lesson 1

--- app/src/main/java/com/br/thiago/diceroller/MainActivity.kt
+++ app/src/main/java/com/br/thiago/diceroller/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.br.thiago.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val diceImage: ImageView by lazy {
+        findViewById(R.id.dice_image)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
     }
+
+    private fun rollDice() {
+        val drawableResouce = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResouce)
+    }
+
 }
